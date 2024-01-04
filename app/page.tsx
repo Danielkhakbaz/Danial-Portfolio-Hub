fff637a22843bcd43a0d7400eac7b3132c36b264
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-01-05 00:21:10 +0330

    feat: Add FavQuote component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import PageTransition from "components/page-transition/page-transition";
 import ImagesGrid from "components/images-grid/images-grid";
+import FavQuote from "components/fav-quote/fav-quote";
+import { Flex } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
     <PageTransition>
-      <ImagesGrid />
+      <Flex flexDirection="column" gap={8}>
+        <ImagesGrid />
+        <FavQuote />
+      </Flex>
     </PageTransition>
   );
 };
---
+++ components/fav-quote/fav-quote.tsx
@@ -0,0 +1,16 @@
+import { Center } from "@chakra-ui/react";
+
+const FavQuote = async () => {
+  return (
+    <Center
+      backgroundColor="coral"
+      color="white"
+      borderRadius={10}
+      paddingY={6}
+    >
+      Fav Quote
+    </Center>
+  );
+};
+
+export default FavQuote;

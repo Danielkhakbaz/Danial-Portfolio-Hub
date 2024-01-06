76c952e96f63832af07a8551e02014c4a8bf1e8b
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-01-06 17:20:34 +0330

    feat: Add links component to the homepage sections

--- app/_components/homepage-section/links-section/links-section.tsx
+++ app/_components/homepage-section/links-section/links-section.tsx
@@ -12,14 +12,11 @@ import { FaTelegramPlane } from "react-icons/fa";
 
 const LinksSection = async () => {
   return (
-    <Flex
-      flexDirection={{ base: "column", md: "row" }}
-      justifyContent="space-between"
-      marginY={4}
-    >
+    <Flex flexWrap="wrap" justifyContent="space-between" gap={4} marginY={4}>
       <Link href="mailto:danialkhakbaz.dk@gmail.com" target="_blank">
-        <Tooltip label="Email me!">
+        <Tooltip hasArrow label="Email me!" backgroundColor="#D3D3D3">
           <FaEnvelope
+            className="homepage__links--hover"
             style={{
               fontSize: 30,
               color: "#D3D3D3",
@@ -28,8 +25,9 @@ const LinksSection = async () => {
         </Tooltip>
       </Link>
       <Link href="https://t.me/danielkhakbaz" target="_blank">
-        <Tooltip label="Telegram">
+        <Tooltip hasArrow label="Telegram" backgroundColor="#0088CC">
           <FaTelegramPlane
+            className="homepage__links--hover"
             style={{
               fontSize: 30,
               color: "#0088CC",
@@ -38,8 +36,9 @@ const LinksSection = async () => {
         </Tooltip>
       </Link>
       <Link href="https://twitter.com/danielkhakbaz" target="_blank">
-        <Tooltip label="Twitter(X)">
+        <Tooltip hasArrow label="Twitter(X)" backgroundColor="#1DA1F2">
           <FaXTwitter
+            className="homepage__links--hover"
             style={{
               fontSize: 30,
               color: "#1DA1F2",
@@ -48,8 +47,9 @@ const LinksSection = async () => {
         </Tooltip>
       </Link>
       <Link href="https://www.instagram.com/danielkhakbaz/" target="_blank">
-        <Tooltip label="Instagram">
+        <Tooltip hasArrow label="Instagram" backgroundColor="#DD2A7B">
           <FaInstagram
+            className="homepage__links--hover"
             style={{
               fontSize: 30,
               color: "#DD2A7B",
@@ -58,18 +58,20 @@ const LinksSection = async () => {
         </Tooltip>
       </Link>
       <Link href="https://github.com/danielkhakbaz" target="_blank">
-        <Tooltip label="Github">
+        <Tooltip hasArrow label="Github" backgroundColor="#818288">
           <FaGithub
+            className="homepage__links--hover"
             style={{
               fontSize: 30,
-              opacity: 0.6,
+              color: "#818288",
             }}
           />
         </Tooltip>
       </Link>
       <Link href="https://www.linkedin.com/in/danielkhakbaz/" target="_blank">
-        <Tooltip label="Linkedin">
+        <Tooltip hasArrow label="Linkedin" backgroundColor="#0077B5">
           <FaLinkedinIn
+            className="homepage__links--hover"
             style={{
               fontSize: 30,
               color: "#0077B5",
@@ -81,8 +83,9 @@ const LinksSection = async () => {
         href="https://open.spotify.com/user/3p0q2i0j7g4e0vo9hc0kkpmov?si=22691cd459ad4736&nd=1"
         target="_blank"
       >
-        <Tooltip label="Spotify">
+        <Tooltip hasArrow label="Spotify" backgroundColor="#1ED760">
           <FaSpotify
+            className="homepage__links--hover"
             style={{
               fontSize: 30,
               color: "#1ED760",

3facf720286ac4e53c8d16a94a2ff2bc2163e630
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-01-06 16:57:54 +0330

    feat: Add the links component for the homepage sections

--- app/_components/homepage-section/links-section/links-section.tsx
+++ app/_components/homepage-section/links-section/links-section.tsx
@@ -1,8 +1,96 @@
+import Link from "next/link";
+import { Flex, Tooltip } from "@chakra-ui/react";
+import {
+  FaEnvelope,
+  FaXTwitter,
+  FaInstagram,
+  FaGithub,
+  FaLinkedinIn,
+  FaSpotify,
+} from "react-icons/fa6";
+import { FaTelegramPlane } from "react-icons/fa";
+
 const LinksSection = async () => {
   return (
-    <>
-      <p>Links</p>
-    </>
+    <Flex
+      flexDirection={{ base: "column", md: "row" }}
+      justifyContent="space-between"
+      marginY={4}
+    >
+      <Link href="mailto:danialkhakbaz.dk@gmail.com" target="_blank">
+        <Tooltip label="Email me!">
+          <FaEnvelope
+            style={{
+              fontSize: 30,
+              color: "#D3D3D3",
+            }}
+          />
+        </Tooltip>
+      </Link>
+      <Link href="https://t.me/danielkhakbaz" target="_blank">
+        <Tooltip label="Telegram">
+          <FaTelegramPlane
+            style={{
+              fontSize: 30,
+              color: "#0088CC",
+            }}
+          />
+        </Tooltip>
+      </Link>
+      <Link href="https://twitter.com/danielkhakbaz" target="_blank">
+        <Tooltip label="Twitter(X)">
+          <FaXTwitter
+            style={{
+              fontSize: 30,
+              color: "#1DA1F2",
+            }}
+          />
+        </Tooltip>
+      </Link>
+      <Link href="https://www.instagram.com/danielkhakbaz/" target="_blank">
+        <Tooltip label="Instagram">
+          <FaInstagram
+            style={{
+              fontSize: 30,
+              color: "#DD2A7B",
+            }}
+          />
+        </Tooltip>
+      </Link>
+      <Link href="https://github.com/danielkhakbaz" target="_blank">
+        <Tooltip label="Github">
+          <FaGithub
+            style={{
+              fontSize: 30,
+              opacity: 0.6,
+            }}
+          />
+        </Tooltip>
+      </Link>
+      <Link href="https://www.linkedin.com/in/danielkhakbaz/" target="_blank">
+        <Tooltip label="Linkedin">
+          <FaLinkedinIn
+            style={{
+              fontSize: 30,
+              color: "#0077B5",
+            }}
+          />
+        </Tooltip>
+      </Link>
+      <Link
+        href="https://open.spotify.com/user/3p0q2i0j7g4e0vo9hc0kkpmov?si=22691cd459ad4736&nd=1"
+        target="_blank"
+      >
+        <Tooltip label="Spotify">
+          <FaSpotify
+            style={{
+              fontSize: 30,
+              color: "#1ED760",
+            }}
+          />
+        </Tooltip>
+      </Link>
+    </Flex>
   );
 };
 

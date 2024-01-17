6eff498de0c3c7b412d7e3168b429f36c8dbff5e
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-01-17 16:32:05 +0330

    feat: Finish the Blog Page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import PageTransition from "components/page-transition/page-transition";
+import NoPosts from "assets/images/no-posts.gif";
+import { Flex, Heading, Center } from "@chakra-ui/react";
 
 const BlogPage = async () => {
-  return <PageTransition>Blog</PageTransition>;
+  return (
+    <PageTransition>
+      <Flex flexDirection="column" gap={6}>
+        <Heading>Blog</Heading>
+        <Flex justifyContent="center">
+          <Image src={NoPosts} alt="" />
+        </Flex>
+        <Center>There&apos;s no post yet :(</Center>
+      </Flex>
+    </PageTransition>
+  );
 };
 
 export default BlogPage;

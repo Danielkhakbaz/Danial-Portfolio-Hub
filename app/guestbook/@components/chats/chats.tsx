9844e2627eb01b5caf2749cf6a953490a2312950
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-01-23 17:14:57 +0330

    feat: Add the complete UI of the chats

---
+++ app/guestbook/@components/chats/chats.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import { colors } from "app/guestbook/@components/chats/@constant/colors";
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+type ChatsProps = {
+  data: {
+    user: {
+      name: string;
+      image: string;
+      email: string;
+    };
+  };
+};
+
+const Chats = async ({ data: { user } }: ChatsProps) => {
+  return (
+    <Flex flexDirection="column" gap={4}>
+      {[1, 2, 3, 4].map((_item, index) => (
+        <Flex key={index} alignItems="flex-start" gap={2}>
+          <Flex>
+            {!user.image ? (
+              <Image
+                width={30}
+                height={30}
+                src={user.image}
+                alt={`${user.name}'s image`}
+                style={{
+                  borderRadius: "100%",
+                }}
+              />
+            ) : (
+              <Text
+                as="span"
+                width={30}
+                height={30}
+                backgroundColor={
+                  colors[Math.floor(Math.random() * colors.length)]
+                }
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                borderRadius="100%"
+              >
+                {user?.name?.slice(0, 1)}
+              </Text>
+            )}
+          </Flex>
+          <Heading fontSize={14} color="gray">
+            {user.name}:{" "}
+            <Text
+              as="span"
+              fontWeight="normal"
+              fontSize={13}
+              color="white"
+              textAlign="justify"
+            >
+              jsadkbakbsjk bkjabkbak jsadkbakbsjk bkjabkbakjsadkbakbsjk
+              bkjabkbakjsadkbakbsjk bkjabkbakjsadkbakbsjk bkjabkbakjsadkbakbsjk
+              bkjabkbakjsadkbakbsjk bkjabkbak
+            </Text>
+          </Heading>
+        </Flex>
+      ))}
+    </Flex>
+  );
+};
+
+export default Chats;
--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -2,6 +2,7 @@ import NextLink from "next/link";
 import { getServerSession } from "next-auth";
 import PageTransition from "components/page-transition/page-transition";
 import Authenticated from "app/guestbook/@components/authenticated/authenticated";
+import Chats from "app/guestbook/@components/chats/chats";
 import Unauthenticated from "app/guestbook/@components/unauthenticated/unauthenticated";
 import { authOptions } from "auth/authOptions";
 import { Flex, Heading, Text, Link } from "@chakra-ui/react";
@@ -24,7 +25,14 @@ const GuestbookPage = async () => {
           </Text>
         </Heading>
         <Flex flexDirection="column" gap={6}>
-          {session ? <Authenticated /> : <Unauthenticated />}
+          {session ? (
+            <>
+              <Authenticated />
+              <Chats data={session as never} />
+            </>
+          ) : (
+            <Unauthenticated />
+          )}
         </Flex>
       </Flex>
     </PageTransition>

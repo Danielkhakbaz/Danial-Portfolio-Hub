a5ce49707751494430f8636059033563d54313ce
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-02-16 14:42:18 +0330

    (refactor): Change the layout of the images-grid component based on both mobile and desktop

--- components/app/images-grid/images-grid.tsx
+++ components/app/images-grid/images-grid.tsx
@@ -1,19 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import DanialPlayingBasketball from "public/images/danial-at-basketball.webp";
 import DanialAtShomal from "public/images/danial-at-shomal.webp";
 import DanialFromWorkAtHome from "public/images/danial-from-work-at-home.webp";
 import DanialAtCafe from "public/images/danial-at-cafe.webp";
-import { Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Flex, useMediaQuery } from "@chakra-ui/react";
 
-const ImagesGrid = async () => {
+const ImagesGrid = () => {
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+
   return (
     <Grid
       height="400px"
-      templateRows="repeat(5, 1fr)"
-      templateColumns="repeat(5, 1fr)"
+      templateRows={{ base: "repeat(6, 1fr)", md: "repeat(5, 1fr)" }}
+      templateColumns={{ base: "repeat(6, 1fr)", md: "repeat(5, 1fr)" }}
       gap={3}
     >
-      <GridItem width="100%" height="100%" gridRow="1 / 6" gridColumn="1 / 3">
+      <GridItem
+        width="100%"
+        height="100%"
+        gridRow={{ base: "1/4", md: "1/6" }}
+        gridColumn={{ base: "1/3", md: "1/3" }}
+      >
         <Flex width="100%" height="100%">
           <Image
             src={DanialAtCafe}
@@ -23,15 +32,22 @@ const ImagesGrid = async () => {
             placeholder="blur"
             priority
             style={{
+              width: "100%",
+              height: "100%",
               objectFit: "cover",
               objectPosition: "center",
               borderTopLeftRadius: 20,
-              borderBottomLeftRadius: 20,
+              borderBottomLeftRadius: isLargerThan768 ? 20 : 0,
             }}
           />
         </Flex>
       </GridItem>
-      <GridItem width="100%" height="100%" gridRow="1 / 4" gridColumn="3 / 5">
+      <GridItem
+        width="100%"
+        height="100%"
+        gridRow={{ base: "4/7", md: "1/4" }}
+        gridColumn={{ base: "1/5", md: "3/5" }}
+      >
         <Flex width="100%" height="100%">
           <Image
             src={DanialPlayingBasketball}
@@ -40,11 +56,20 @@ const ImagesGrid = async () => {
             alt="Danial Khakbaz playing basketball"
             placeholder="blur"
             priority
-            style={{ objectFit: "cover", objectPosition: "45% 0" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              objectPosition: isLargerThan768 ? "45% 0" : "center",
+              borderBottomLeftRadius: isLargerThan768 ? 0 : 20,
+            }}
           />
         </Flex>
       </GridItem>
-      <GridItem gridRow="4 / 6" gridColumn="3 / 6">
+      <GridItem
+        gridRow={{ base: "1/4", md: "4/6" }}
+        gridColumn={{ base: "3/7", md: "3/6" }}
+      >
         <Flex width="100%" height="100%">
           <Image
             src={DanialAtShomal}
@@ -54,14 +79,20 @@ const ImagesGrid = async () => {
             placeholder="blur"
             priority
             style={{
+              width: "100%",
+              height: "100%",
               objectFit: "cover",
-              objectPosition: "0 80%",
-              borderBottomRightRadius: 20,
+              objectPosition: isLargerThan768 ? "0 75%" : "center",
+              borderTopRightRadius: isLargerThan768 ? 0 : 20,
+              borderBottomRightRadius: isLargerThan768 ? 20 : 0,
             }}
           />
         </Flex>
       </GridItem>
-      <GridItem gridRow="1 / 4" gridColumn="5 / 6">
+      <GridItem
+        gridRow={{ base: "4/7", md: "1/4" }}
+        gridColumn={{ base: "5/7", md: "5/6" }}
+      >
         <Flex width="100%" height="100%">
           <Image
             src={DanialFromWorkAtHome}
@@ -71,9 +102,12 @@ const ImagesGrid = async () => {
             placeholder="blur"
             priority
             style={{
+              width: "100%",
+              height: "100%",
               objectFit: "cover",
               objectPosition: "center",
-              borderTopRightRadius: 20,
+              borderTopRightRadius: isLargerThan768 ? 20 : 0,
+              borderBottomRightRadius: isLargerThan768 ? 0 : 20,
             }}
           />
         </Flex>

3326eef57fb4c851f51cf1d846b698b7bee13bae
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-02-16 16:39:33 +0330

    (performance): Disable framer-motion animation for the mobiles

--- utils/page-transition/page-transition.tsx
+++ utils/page-transition/page-transition.tsx
@@ -1,22 +1,31 @@
+"use client";
+
 import { ReactNode } from "react";
 import MotionComponent from "utils/motion/motion";
+import { useMediaQuery } from "@chakra-ui/react";
 
 type PageTransitionProps = {
   children: ReactNode;
 };
 
-const PageTransition = async ({ children }: PageTransitionProps) => {
-  return (
-    <MotionComponent
-      tag="div"
-      initial={{ y: 20, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.25 }}
-      style={{ width: "100%", display: "inline-block" }}
-    >
-      {children}
-    </MotionComponent>
-  );
+const PageTransition = ({ children }: PageTransitionProps) => {
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+
+  if (isLargerThan768) {
+    return (
+      <MotionComponent
+        tag="div"
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.25 }}
+        style={{ width: "100%", display: "inline-block" }}
+      >
+        {children}
+      </MotionComponent>
+    );
+  } else {
+    return children;
+  }
 };
 
 export default PageTransition;

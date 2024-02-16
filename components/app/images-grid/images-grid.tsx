616569844e17f932510705e791e454364678149b
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-02-16 14:02:46 +0330

    (refactor): Change the background-image to image from the next.js

--- components/app/images-grid/images-grid.tsx
+++ components/app/images-grid/images-grid.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import DanialPlayingBasketball from "public/images/danial-at-basketball.webp";
 import DanialAtShomal from "public/images/danial-at-shomal.webp";
 import DanialFromWorkAtHome from "public/images/danial-from-work-at-home.webp";
 import DanialAtCafe from "public/images/danial-at-cafe.webp";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Flex, Grid, GridItem } from "@chakra-ui/react";
 
 const ImagesGrid = async () => {
   return (
@@ -10,49 +11,73 @@ const ImagesGrid = async () => {
       height="400px"
       templateRows="repeat(5, 1fr)"
       templateColumns="repeat(5, 1fr)"
-      gap={4}
+      gap={3}
     >
-      <GridItem
-        gridRow="1 / 6"
-        gridColumn="1 / 3"
-        borderTopLeftRadius={20}
-        borderBottomLeftRadius={20}
-        style={{
-          backgroundImage: `url(${DanialAtCafe.src})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      />
-      <GridItem
-        gridRow="1 / 4"
-        gridColumn="3 / 5"
-        style={{
-          backgroundImage: `url(${DanialPlayingBasketball.src})`,
-          backgroundSize: "cover",
-          backgroundPosition: "45% 0",
-        }}
-      />
-      <GridItem
-        gridRow="4 / 6"
-        gridColumn="3 / 6"
-        borderBottomRightRadius={20}
-        style={{
-          backgroundImage: `url(${DanialAtShomal.src})`,
-          backgroundSize: "cover",
-          backgroundPosition: "0 80%",
-        }}
-      />
-      <GridItem
-        gridRow="1 / 4"
-        gridColumn="5 / 6"
-        borderTopRightRadius={20}
-        style={{
-          backgroundImage: `url(${DanialFromWorkAtHome.src})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      />
+      <GridItem width="100%" height="100%" gridRow="1 / 6" gridColumn="1 / 3">
+        <Flex width="100%" height="100%">
+          <Image
+            src={DanialAtCafe}
+            width={233}
+            height={400}
+            alt="Danial Khakbaz at cafe"
+            placeholder="blur"
+            priority
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+            }}
+          />
+        </Flex>
+      </GridItem>
+      <GridItem width="100%" height="100%" gridRow="1 / 4" gridColumn="3 / 5">
+        <Flex width="100%" height="100%">
+          <Image
+            src={DanialPlayingBasketball}
+            width={233}
+            height={233}
+            alt="Danial Khakbaz playing basketball"
+            placeholder="blur"
+            priority
+            style={{ objectFit: "cover", objectPosition: "45% 0" }}
+          />
+        </Flex>
+      </GridItem>
+      <GridItem gridRow="4 / 6" gridColumn="3 / 6">
+        <Flex width="100%" height="100%">
+          <Image
+            src={DanialAtShomal}
+            width={358}
+            height={150}
+            alt="Danial Khakbaz at the north of Iran"
+            placeholder="blur"
+            priority
+            style={{
+              objectFit: "cover",
+              objectPosition: "0 80%",
+              borderBottomRightRadius: 20,
+            }}
+          />
+        </Flex>
+      </GridItem>
+      <GridItem gridRow="1 / 4" gridColumn="5 / 6">
+        <Flex width="100%" height="100%">
+          <Image
+            src={DanialFromWorkAtHome}
+            width={120}
+            height={233}
+            alt="Danial Khakbaz working from home"
+            placeholder="blur"
+            priority
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+              borderTopRightRadius: 20,
+            }}
+          />
+        </Flex>
+      </GridItem>
     </Grid>
   );
 };

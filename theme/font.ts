d81ea43264fd761b853441a49e810bab083b2f53
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-02-17 13:11:08 +0330

    (performance): Remove the unused fonts

--- theme/font.ts
+++ theme/font.ts
@@ -1,7 +1,7 @@
 import { Lato } from "next/font/google";
 
 export const LatoFont = Lato({
-  subsets: ["latin", "latin-ext"],
-  style: ["normal", "italic"],
+  subsets: ["latin"],
+  style: ["normal"],
   weight: ["100", "300", "400", "700", "900"],
 });
